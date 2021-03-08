bc089af0efc75efe3707fe98ed1bb3c96b45469e
Andrew Gasson <56293604+andrewgasson@users.noreply.github.com>
2021-03-08 19:51:09 +1030

    Added Vector2 ordinals

--- Source/Game/Compass.c
+++ Source/Game/Compass.c
@@ -156,12 +156,16 @@ Vector2 CompassToVector2(Compass compass)
 {
 	// Compass is relative to screen (where 0,0 is top,left)
 	switch (compass) {
-	case COMPASS_NORTH: return (Vector2) {  0, -1 };
-	case COMPASS_EAST:  return (Vector2) {  1,  0 };
-	case COMPASS_SOUTH: return (Vector2) {  0,  1 };
-	case COMPASS_WEST:  return (Vector2) { -1,  0 };
+	case COMPASS_NORTH:      return (Vector2) {  0, -1 };
+	case COMPASS_NORTH_EAST: return (Vector2) {  1, -1 };
+	case COMPASS_EAST:       return (Vector2) {  1,  0 };
+	case COMPASS_SOUTH_EAST: return (Vector2) {  1,  1 };
+	case COMPASS_SOUTH:      return (Vector2) {  0,  1 };
+	case COMPASS_SOUTH_WEST: return (Vector2) { -1,  1 };
+	case COMPASS_WEST:       return (Vector2) { -1,  0 };
+	case COMPASS_NORTH_WEST: return (Vector2) { -1, -1 };
 	default:
-		TraceLog(LOG_ERROR, TextFormat("COMPASS: CompassToVector2 conversion for \"%d\" not implemented", compass));
+		TraceLog(LOG_ERROR, TextFormat("COMPASS: Vector2 conversion for value \"%d\" is not implemented", compass));
 		return (Vector2) { 0, 0 };
 	}
 }
